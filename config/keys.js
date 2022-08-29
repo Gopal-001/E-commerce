18d9c72fc52930f965839372e7433ece17c0ee41
Gopal-001 <sameeemishra@gmail.com>
2022-08-29 22:49:47 +0530

    Added all files.

---
+++ config/keys.js
@@ -0,0 +1,18 @@
+// add to .gitignore
+const dotenv = require('dotenv');
+dotenv.config();
+const user = process.env.user;
+const pass = process.env.secret;
+const my_url = process.env.url;
+module.exports = {
+    google : {
+        clientId : "<url>.apps.googleusercontent.com",
+        clientSecret : "my-secret"
+    },
+    mongoDB : {
+        dbURI : 'mongodb+srv://'+user+':'+pass+'@'+my_url+'?retryWrites=true&w=majority'
+    },
+    session : {
+        cookieKey : 'rochakssecret'
+    }
+}
